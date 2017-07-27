70c2960b41d30dcbcba155a9e434c1f893541605
Jace Ju <jaceju@gmail.com>
2017-07-27 14:35:52 +0800

    Fix coding style

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -7,9 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-	public function testTrue()
-	{
-		$target = new Example();
-		$this->assertInstanceOf(Example::class, $target);
-	}
+    public function testTrue()
+    {
+        $target = new Example();
+        $this->assertInstanceOf(Example::class, $target);
+    }
 }
