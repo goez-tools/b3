51d3ed5a02d40d26e80604cec8de424e99a36e08
Jace Ju <jaceju@kkbox.com>
2020-02-18 11:37:40 +0800

    Update test example

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,14 +2,41 @@
 
 namespace Tests;
 
+use Mockery;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Ns\Example;
 use PHPUnit\Framework\TestCase;
 
+// Import the mockery helpers: mock(), spy(), ...
+Mockery::globalHelpers();
+
 class ExampleTest extends TestCase
 {
-    public function testInstanceOf()
+    use MockeryPHPUnitIntegration;
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_be_a_instance_of_example_class(): void
     {
         $target = new Example();
         $this->assertInstanceOf(Example::class, $target);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_spy_a_closure(): void
+    {
+        $spy = spy(function () {});
+
+        call_user_func($spy);
+
+        $spy->shouldHaveBeenCalled()->once();
     }
 }
